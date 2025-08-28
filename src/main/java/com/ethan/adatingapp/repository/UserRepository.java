a2d22a02500e72a28b73bd7aaf0e80390922e023
miss-puse <sejahope63@gmail.com>
2025-08-28 10:11:18 +0200

    Update UserRepository.java

--- src/main/java/com/ethan/adatingapp/repository/UserRepository.java
+++ src/main/java/com/ethan/adatingapp/repository/UserRepository.java
@@ -1,26 +1,27 @@
 package com.ethan.adatingapp.repository;
 
 import com.ethan.adatingapp.domain.User;
-import com.ethan.adatingapp.domain.enums.Course;
-import com.ethan.adatingapp.domain.enums.Gender;
-import com.ethan.adatingapp.domain.enums.Institution;
-import com.ethan.adatingapp.domain.enums.Interest;
+import com.ethan.adatingapp.domain.enums.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-    User findByUsernameAndPassword(String username, String password);
+public interface UserRepository extends JpaRepository<User, Long> {
 
+    // For bcrypt login
+    User findByUsername(String username);
+
+    // Filtering
     List<User> findAllByCourse(Course course);
     List<User> findAllByInstitution(Institution institution);
     List<User> findAllByAgeBetween(int minAge, int maxAge);
     List<User> findAllByGender(Gender gender);
     List<User> findAllByInterestsIn(Collection<Interest> interests);
 
-    User findByEmail(String email);
-
+    // For scheduled deletion
+    List<User> findAllByDeletionDueDateBefore(LocalDateTime time);
 }
