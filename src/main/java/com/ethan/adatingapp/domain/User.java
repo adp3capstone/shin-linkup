efb302eb39401d89b5bfbd7568ea7e1218c514bd
Seja.Kgomokaboya <sejahope63@gmail.com>
2025-09-11 13:57:35 +0200

    updated

--- src/main/java/com/ethan/adatingapp/domain/User.java
+++ src/main/java/com/ethan/adatingapp/domain/User.java
@@ -2,9 +2,6 @@ package com.ethan.adatingapp.domain;
 
 import com.ethan.adatingapp.domain.enums.*;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,19 +19,12 @@ public class User {
     private LocalDateTime deletionDueDate;
 
     private String firstName;
-
     private String lastName;
-
     private String email;
-
     private String username;
-
     private String password;
-
     private int age;
-
     private String bio;
-
     private Institution institution;
     private Gender gender;
     private Course course;
@@ -59,24 +49,14 @@ public class User {
 
     @OneToMany(mappedBy = "liked", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Like> likesReceived;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private EmergencyContact emergencyContact;
 
-    public Set<Like> getLikesGiven() {
-        return likesGiven;
-    }
+    // Default constructor
+    public User() {}
 
-    public void setLikesGiven(Set<Like> likesGiven) {
-        this.likesGiven = likesGiven;
-    }
-
-    public Set<Like> getLikesReceived() {
-        return likesReceived;
-    }
-
-    public void setLikesReceived(Set<Like> likesReceived) {
-        this.likesReceived = likesReceived;
-    }
-
-        private User(Builder builder) {
+    // Builder constructor
+    private User(Builder builder) {
         this.userId = builder.userId;
         this.username = builder.username;
         this.password = builder.password;
@@ -85,83 +65,64 @@ public class User {
         this.lastName = builder.lastName;
         this.age = builder.age;
         this.bio = builder.bio;
-
         this.institution = builder.institution;
         this.gender = builder.gender;
+        this.course = builder.course;
         this.interests = builder.interests;
-
         this.image = builder.image;
+        this.deletionDueDate = builder.deletionDueDate;
+        this.emergencyContact = builder.emergencyContact;
+
+        // Maintain bidirectional relationship
+        if (this.emergencyContact != null) {
+            this.emergencyContact.setUser(this);
+        }
     }
 
-    public User() {
+    // Getters and Setters
+    public Long getUserId() { return userId; }
+    public LocalDateTime getDeletionDueDate() { return deletionDueDate; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getEmail() { return email; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public int getAge() { return age; }
+    public String getBio() { return bio; }
+    public Institution getInstitution() { return institution; }
+    public Gender getGender() { return gender; }
+    public Course getCourse() { return course; }
+    public List<Interest> getInterests() { return interests; }
+    public Image getImage() { return image; }
+    public Preference getPreferences() { return preferences; }
+    public Set<Like> getLikesGiven() { return likesGiven; }
+    public Set<Like> getLikesReceived() { return likesReceived; }
+    public EmergencyContact getEmergencyContact() { return emergencyContact; }
 
+    public void setUserId(Long userId) { this.userId = userId; }
+    public void setDeletionDueDate(LocalDateTime deletionDueDate) { this.deletionDueDate = deletionDueDate; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setEmail(String email) { this.email = email; }
+    public void setUsername(String username) { this.username = username; }
+    public void setPassword(String password) { this.password = password; }
+    public void setAge(int age) { this.age = age; }
+    public void setBio(String bio) { this.bio = bio; }
+    public void setInstitution(Institution institution) { this.institution = institution; }
+    public void setGender(Gender gender) { this.gender = gender; }
+    public void setCourse(Course course) { this.course = course; }
+    public void setInterests(List<Interest> interests) { this.interests = interests; }
+    public void setImage(Image image) { this.image = image; }
+    public void setPreferences(Preference preferences) { this.preferences = preferences; }
+    public void setLikesGiven(Set<Like> likesGiven) { this.likesGiven = likesGiven; }
+    public void setLikesReceived(Set<Like> likesReceived) { this.likesReceived = likesReceived; }
+
+    public void setEmergencyContact(EmergencyContact emergencyContact) {
+        this.emergencyContact = emergencyContact;
+        if (emergencyContact != null) {
+            emergencyContact.setUser(this); // maintain bidirectional consistency
+        }
     }
-
-    public LocalDateTime getDeletionDueDate() {
-        return deletionDueDate;
-    }
-
-    public Course getCourse() {
-        return course;
-    }
-
-    public void setCourse(Course course) {
-        this.course = course;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-
-    public Institution getInstitution() {
-        return institution;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public List<Interest> getInterests() {
-        return interests;
-    }
-
-    public Image getImage() {
-        return image;
-    }
-
-    public Preference getPreferences() {
-        return preferences;
-    }
-
 
     @Override
     public String toString() {
@@ -172,74 +133,77 @@ public class User {
                 ", lastName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
                 ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
                 ", age=" + age +
                 ", bio='" + bio + '\'' +
                 ", institution=" + institution +
                 ", gender=" + gender +
                 ", course=" + course +
                 ", interests=" + interests +
-                ", image=" + image +
-                ", preferences=" + preferences +
-                ", likesGiven=" + likesGiven +
-                ", likesReceived=" + likesReceived +
+                ", emergencyContact=" + (emergencyContact != null ? emergencyContact.getFirstName() : null) +
                 '}';
     }
 
-    //Setters
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
+    // Builder Pattern
+    public static class Builder {
+        private Long userId;
+        private LocalDateTime deletionDueDate;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String username;
+        private String password;
+        private int age;
+        private String bio;
+        private Institution institution;
+        private Gender gender;
+        private Course course;
+        private List<Interest> interests;
+        private Image image;
+        private EmergencyContact emergencyContact;
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+        public Builder setUserId(Long userId) { this.userId = userId; return this; }
+        public Builder setDeletionDueDate(LocalDateTime deletionDueDate) { this.deletionDueDate = deletionDueDate; return this; }
+        public Builder setFirstName(String firstName) { this.firstName = firstName; return this; }
+        public Builder setLastName(String lastName) { this.lastName = lastName; return this; }
+        public Builder setEmail(String email) { this.email = email; return this; }
+        public Builder setUsername(String username) { this.username = username; return this; }
+        public Builder setPassword(String password) { this.password = password; return this; }
+        public Builder setAge(int age) { this.age = age; return this; }
+        public Builder setBio(String bio) { this.bio = bio; return this; }
+        public Builder setInstitution(Institution institution) { this.institution = institution; return this; }
+        public Builder setGender(Gender gender) { this.gender = gender; return this; }
+        public Builder setCourse(Course course) { this.course = course; return this; }
+        public Builder setInterests(List<Interest> interests) { this.interests = interests; return this; }
+        public Builder setImage(Image image) { this.image = image; return this; }
+        public Builder setEmergencyContact(EmergencyContact emergencyContact) {
+            this.emergencyContact = emergencyContact;
+            if (emergencyContact != null) {
+                emergencyContact.setUser(null); // will be set in User constructor
+            }
+            return this;
+        }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+        public Builder copy(User user) {
+            this.userId = user.userId;
+            this.deletionDueDate = user.deletionDueDate;
+            this.firstName = user.firstName;
+            this.lastName = user.lastName;
+            this.email = user.email;
+            this.username = user.username;
+            this.password = user.password;
+            this.age = user.age;
+            this.bio = user.bio;
+            this.institution = user.institution;
+            this.gender = user.gender;
+            this.course = user.course;
+            this.interests = user.interests;
+            this.image = user.image;
+            this.emergencyContact = user.emergencyContact;
+            return this;
+        }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public void setInstitution(Institution institution) {
-        this.institution = institution;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public void setInterests(List<Interest> interests) {
-        this.interests = interests;
-    }
-
-    public void setImage(Image image) {
-        this.image = image;
-    }
-
-    public void setPreferences(Preference preferences) {
-        this.preferences = preferences;
-    }
-  
-    public void setDeletionDueDate(LocalDateTime deletionDueDate) {
-        this.deletionDueDate = deletionDueDate;
+        public User build() {
+            return new User(this);
+        }
     }
 }
